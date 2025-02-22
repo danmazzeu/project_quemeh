98cb1daf388be4fad2a985cc773f5f3bf2d4010f
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-22 10:56:48 -0300

    Upload

--- js/protect.js
+++ js/protect.js
@@ -4,3 +4,15 @@ document.addEventListener('contextmenu', (event) => {
 
 console.log("Elaborado e Desenvolvido por Daniel Mazzeu.");
 console.log("Todos os direitos reservados " + new Date().getFullYear() + ".");
+
+var testAd = document.createElement('div');
+testAd.innerHTML = '&nbsp;';
+testAd.className = 'ad-banner';
+document.body.appendChild(testAd);
+
+setTimeout(function() {
+    if (testAd.offsetHeight === 0) {
+        alert('Por favor, desative o AdBlock para acessar nosso site!');
+    }
+    testAd.remove();
+}, 100);
